87890959b054a146405208f8d29b79252a3c0709
Dananjaya Polwattage <dananjayapolwattage@gmail.com>
2023-06-11 12:02:51 +0530

    Add Expense
    Add Expense component added

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,20 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 
 function ExpenseItem(data) {
-  const expenseTitle = data.title;
+  const [titleState, setTitleState] = useState(data.title);
+
   const expenseAmount = data.amount;
   const expenseDate = data.date;
+
+  const onClickChangeTitle = () => {
+    setTitleState("Updated!");
+  };
 
   return (
     <Card className="expense-item">
       <ExpenseDate date={expenseDate} />
       <div className="expense-item__description">
-        <h2>{expenseTitle}</h2>
+        <h2>{titleState}</h2>
         <div className="expense-item__price">${expenseAmount}</div>
       </div>
+      <button onClick={onClickChangeTitle}>Change Title</button>
     </Card>
   );
 }
