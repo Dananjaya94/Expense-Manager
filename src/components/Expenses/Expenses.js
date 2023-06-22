e74d353a184d316e3d3f008d3b887d30c8f0891d
Dananjaya Polwattage <dananjayapolwattage@gmail.com>
2023-06-22 12:08:21 +0530

    Expense filter
    Selected year functionality added

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -5,18 +5,33 @@ import Card from "../UI/Card";
 import ExpensesFilter from "./ExpensesFilter";
 
 export default function Expenses(prop) {
-  const [selectYear, setSelectedYear] = useState("2021");
+  const [filteredYear, setFilteredYear] = useState("2020");
+  let filterInfoText = "2019, 2021 & 2022";
+
+  if (filteredYear === "2019") {
+    filterInfoText = "2020, 2021 & 2022";
+  } else if (filteredYear === "2020") {
+    filterInfoText = "2019, 2021 & 2022";
+  } else if (filteredYear === "2021") {
+    filterInfoText = "2019, 2020 & 2022";
+  } else if (filteredYear === "2022") {
+    filterInfoText = "2019, 2020 & 2021";
+  }
 
   const onSelectExpenseYear = (selectedYear) => {
     console.log("Expense.js");
-    setSelectedYear(selectedYear);
-    console.log(selectYear);
+    setFilteredYear(selectedYear);
+    console.log(selectedYear);
   };
 
   return (
     <div>
       <Card className="expenses">
-        <ExpensesFilter onSelectYear={onSelectExpenseYear} />
+        <ExpensesFilter
+          defaultYear={filteredYear}
+          onSelectYear={onSelectExpenseYear}
+        />
+        <p>Data for year {filterInfoText} is hidden.</p>
         <ExpenseItem
           title={prop.expenses[0].title}
           amount={prop.expenses[0].amount}
